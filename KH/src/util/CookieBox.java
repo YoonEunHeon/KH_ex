615a0024d7f0e4f4ea941566be69b09a90796984
eun heon <eun heon@192.168.0.18>
2018-02-13 15:12:09 +0900

    Register Beans/CookieBox

--- KH/src/util/CookieBox.java
+++ KH/src/util/CookieBox.java
@@ -4,26 +4,44 @@ package util;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.Cookie;
 import java.util.Map;
-import java.net.URLEncoder;
-import java.net.URLDecoder;
-import java.io.IOException;
+import java.net.URLEncoder; 
+import java.net.URLDecoder; 
+import java.io.IOException; 
 
 public class CookieBox {
-	
-	private Map<String,Cookie> cookieMap 
-	= new java.util.HashMap<String, Cookie>();
-	
-	public CookieBox(HttpServletRequest request) {
-		Cookie[] cookies = request.getCookies();
-		if(cookies != null){
-			for(int i=0; i<cookies.length ; i++) {
-				cookieMap.put(cookies[i].getName(), cookies[i]);
-				
-			}			
-			
-		}
-	}
-	public static Cookie createCookie(String name, String value) throws IOException{
-		
-	}
+    
+    private Map<String, Cookie> cookieMap 
+    = new java.util.HashMap<String, Cookie>();
+    
+    public CookieBox(HttpServletRequest request) {
+        Cookie[] cookies = request.getCookies();
+        if (cookies != null) {
+            for (int i = 0 ; i < cookies.length ; i++) {
+                cookieMap.put(cookies[i].getName(), cookies[i]);
+            }
+        }
+    }
+    
+    public static Cookie createCookie(String name, String value)
+    throws IOException {
+        return new Cookie(name, URLEncoder.encode(value, "euc-kr"));
+    }
+
+    public static Cookie createCookie(String name, String value, String path, 
+        int maxAge) throws IOException {
+        Cookie cookie = new Cookie(name, URLEncoder.encode(value, "euc-kr"));
+        cookie.setPath(path);
+        cookie.setMaxAge(maxAge);
+        return cookie;
+    }
+    public static Cookie createCookie(String name, String value, String domain,
+            String path, int maxAge) throws IOException {
+        Cookie cookie = new Cookie(name, URLEncoder.encode(value, "euc-kr"));
+        cookie.setDomain(domain);
+        cookie.setPath(path);
+        cookie.setMaxAge(maxAge);
+        return cookie;
+        
+        
+    }
 }
